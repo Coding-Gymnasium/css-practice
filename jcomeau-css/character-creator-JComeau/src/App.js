1af5e728a6739fac72c0ef520e2694058747cd08
Nico Rithner <nicorithner@gmail.com>
2023-02-27 13:13:53 -0700

    Change perspective solution to use JC solution

--- jcomeau-css/character-creator-JComeau/src/App.js
+++ jcomeau-css/character-creator-JComeau/src/App.js
@@ -2,13 +2,11 @@ import React from "react";
 
 import CharacterEditor from "./components/CharacterEditor";
 import Footer from "./components/Footer";
-import "./App.css";
 
 function App() {
   return (
     <>
       <CharacterEditor />
-      <div className="perspective-decoration"></div>
       <Footer />
     </>
   );
